05d49b932b39f83d64864d49c866a9929697d218
Deepali Kadway <dkadway@gmail.com>
2025-05-28 19:34:16 -0600

    initial commit

---
+++ CI Calculator/script.js
@@ -0,0 +1,34 @@
+function calculate(){
+    const principalAmount = document.getElementById("principalAmount");
+    const interestAmount = document.getElementById("interestAmount");
+    const years = document.getElementById("years");
+    const amount = document.getElementById("amount");
+
+    let principal = Number(principalAmount.value)
+    let interest = Number(interestAmount.value / 100)
+    let numofyears = Number(years.value)
+
+    if(principal < 0 || isNaN(principal))
+    {
+        principal = 0
+        principalAmount.value = 0;
+    }
+    if(interest < 0 || isNaN(interest))
+    {   
+        interest = 0
+        interestAmount.value = 0;
+    }
+    if(numofyears < 0 || isNaN(numofyears))
+    {   
+        numofyears = 0
+        years.value = 0;
+    }
+
+    const total = principal * Math.pow((1 + interest / 1), 1 * numofyears)
+    amount.textContent = total.toLocaleString(undefined,
+                                               {style: "currency", 
+                                                currency: "CAD"
+                                               } 
+    )
+
+}
